a3218d25360cc3d51b4e028dcbf260c4db432b7a
dorian-tachet <dorian.tachet@etu.univ-amu.fr>
2018-12-05 11:52:18 +0100

    Ajout du décorateur Apiculteur(pour produire des produits d'apiculture) et du producteur de viande (pour produire de la viande)

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Producteur/ProducteurDeViande.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Producteur/ProducteurDeViande.java
@@ -1,4 +1,20 @@
 package fr.univ.amu.marcheFermier.PHRRT.Donnée.Producteur;
 
-public class ProducteurDeViande {
+import fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit.Viande;
+
+public class ProducteurDeViande extends Producteur implements DecorateurProducteur {
+
+    private Producteur producteurADecorer;
+
+
+    public ProducteurDeViande(Producteur producteurADecorer) {
+        this.producteurADecorer = producteurADecorer;
+    }
+
+    @Override
+    public void produire() {
+        producteurADecorer.produire();
+        System.out.println("Je produis de la bonne viande");
+
+    }
 }
