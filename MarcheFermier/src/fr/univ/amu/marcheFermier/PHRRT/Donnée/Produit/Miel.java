3b3dae197e43d66d8a831780cd004322ae7d5772
dorian-tachet <dorian.tachet@etu.univ-amu.fr>
2018-12-05 11:54:05 +0100

    début de l'ajout des classe concrète de produit

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/Miel.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/Miel.java
@@ -2,7 +2,7 @@ package fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit;
 
 import java.time.LocalDate;
 
-public class Miel extends ProduitFermier {
+public class Miel extends ProduitApiculture {
 
 
     public Miel(LocalDate peremptionDate, double prix) {
---
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/Viande.java
@@ -0,0 +1,9 @@
+package fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit;
+
+import java.time.LocalDate;
+
+public abstract class Viande  extends ProduitFermier{
+    public Viande(LocalDate peremptionDate, double prix) {
+        super(peremptionDate, prix);
+    }
+}
