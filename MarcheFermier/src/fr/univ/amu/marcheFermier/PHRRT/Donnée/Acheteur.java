17614c43d90dc257397b04e7e8a789c4885b2d91
dorian-tachet <dorian.tachet@etu.univ-amu.fr>
2018-12-05 11:53:04 +0100

    Le producteur devient un acheteur pour pouvoir acheter sur le marché

---
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Acheteur.java
@@ -0,0 +1,11 @@
+package fr.univ.amu.marcheFermier.PHRRT.Donnée;
+
+public abstract class Acheteur {
+
+    private double money;
+
+
+    public double getMoney() {
+        return money;
+    }
+}
--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Producteur/Producteur.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Producteur/Producteur.java
@@ -1,13 +1,22 @@
 package fr.univ.amu.marcheFermier.PHRRT.Donnée.Producteur;
 
+import fr.univ.amu.marcheFermier.PHRRT.Donnée.Acheteur;
 import fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit.ProduitFermier;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class Producteur {
+public class Producteur extends Acheteur {
 
     private List<ProduitFermier> stock = new ArrayList<>();
 
-    
+
+    public  List<ProduitFermier> getStock() {
+        return stock;
+    }
+
+
+    public void produire() {
+        System.out.println("Je produis..");
+    }
 }
