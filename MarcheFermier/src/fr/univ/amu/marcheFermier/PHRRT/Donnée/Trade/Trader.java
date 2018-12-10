c3035c28278e83a7e0bea02189e35adfe6af5d21
axel-ribes <ri77ax34&*>
2018-12-10 17:21:47 +0100

    modif trade

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Trade/Trader.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Trade/Trader.java
@@ -1,4 +1,8 @@
 package fr.univ.amu.marcheFermier.PHRRT.Donnée.Trade;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Trader {
+    private HashMap<Client,Double> ListeDeMesClients = new HashMap<>();
 }
