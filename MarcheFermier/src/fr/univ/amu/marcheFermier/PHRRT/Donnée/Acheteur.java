5b6f90d81731b44098fc51a859465bc668a7a59c
dorian-tachet <dorian.tachet@etu.univ-amu.fr>
2018-12-12 10:22:56 +0100

    l'acheteur peut désormais stocker les produits qu'il achete

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Acheteur.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Acheteur.java
@@ -1,10 +1,20 @@
 package fr.univ.amu.marcheFermier.PHRRT.Donnée;
 
-public abstract class Acheteur {
+import fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit.ProduitFermier;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class Acheteur {
+
+
+    private List<ProduitFermier> stock = new ArrayList<>();
     private double money;
 
 
+    public  List<ProduitFermier> getStock() {
+        return stock;
+    }
     public double getMoney() {
         return money;
     }
