ea1c7eab7a74b3000970a9088d69cf759cee45bc
dorian-tachet <dorian.tachet@etu.univ-amu.fr>
2018-12-19 16:46:08 +0100

    Refonte des produits ils leur faut desormais un acheteur qui sera leur proprietaire

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitBio.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitBio.java
@@ -1,12 +1,14 @@
 package fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit;
 
+import fr.univ.amu.marcheFermier.PHRRT.Donnée.Acheteur;
+
 import java.time.LocalDate;
 
 public abstract class ProduitBio extends ProduitFermier {
 
 
-    public ProduitBio(LocalDate peremptionDate,String name, int amount) {
-        super(peremptionDate,name, amount);
+    public ProduitBio(LocalDate peremptionDate, String name, int amount, Acheteur proprietaire) {
+        super(peremptionDate,name, amount, proprietaire);
     }
 }
 
