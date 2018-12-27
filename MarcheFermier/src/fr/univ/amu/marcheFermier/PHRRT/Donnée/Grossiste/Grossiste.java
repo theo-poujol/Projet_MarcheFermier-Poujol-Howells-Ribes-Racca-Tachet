05303015d7478e60706741917c3ea53976fc93f4
theo-poujol <theo.poujol@etu.univ-amu.fr>
2018-12-27 18:59:49 +0100

    changements

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Grossiste/Grossiste.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Grossiste/Grossiste.java
@@ -8,6 +8,7 @@ import fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit.ProduitFermier;
 import fr.univ.amu.marcheFermier.PHRRT.Donnée.Trade.PropositionVente;
 import fr.univ.amu.marcheFermier.PHRRT.Exception.NotEnoughCapacityException;
 import fr.univ.amu.marcheFermier.PHRRT.Exception.NotEnoughtMoneyException;
+import fr.univ.amu.marcheFermier.PHRRT.Exception.NotFoundException;
 
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
@@ -24,24 +25,27 @@ public class Grossiste extends Acheteur {
 
 
 
-    public void sellMyProduct(ProduitFermier product, int cap) {
+    // Solution pour palier au problème de grosse quantité, lorsque l'on veut vendre seulement une partie d'un gros stock de quantité
+    // On crée donc un nouveau ProduitEncheres qui est un ProduitFermier. On a donc deux produits différents car ils different de leur quantité
+    // On a donc le ProduitFermier au quel on a enlevé la quantité mise en vente voulue
+    // Et le ProduitEnchere qui est aux enchères avec la quantité voulue.
 
+    public void sellMyProduct(ProduitFermier product, int cap) {
         try {
             for (ProduitFermier p : sellProducts) {
                 if (sellProducts.contains(product)) {
-                    if (product.getQuantite() < cap) throw new NotEnoughCapacityException();
+                    if (product.getQuantite() < cap) throw new NotEnoughCapacityException(); // Exception sur la quantité
                     else {
                         ProduitEncheres pe = new ProduitEncheres(product,cap);
                         PropositionVente pv = new PropositionVente(this,pe,pe.getQuantite());
                         product.setQuantite(product.getQuantite() - cap);
                     }
-
                 }
+                else throw new NotFoundException();
             }
         }
-
-        catch (NotEnoughCapacityException e) { e.getMessage(); }
-
+        catch (NotEnoughCapacityException e) {e.getMessage();}
+        catch (NotFoundException e) {e.getMessage();}
     }
 
     public void buyProduct() {
