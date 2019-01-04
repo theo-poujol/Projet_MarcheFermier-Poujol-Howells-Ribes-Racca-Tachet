af24f2a85ba2aa536208e87e79f6c3b93507e91a
dorianosaure <dorianosaure@hotmail.fr>
2019-01-04 18:41:30 +0100

    modification de la méthode d'ajout des produits

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Producteur/Producteur.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Producteur/Producteur.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class Producteur extends Acheteur {
 
     private boolean bio = false;
-    private int capacite = 50;
+    private int capacite = 50000000;
 
     public Producteur(double money, String pseudo) {
         super(money, pseudo);
@@ -33,24 +33,23 @@ public class Producteur extends Acheteur {
 
         //si il n'y a plus de place on n'ajoute pas
         if (capaciteRestante == 0) return;
-        //si le produit est deja en possession du producteur
 
-        if (!addExistingProduit(produitFermier)) {
-            //si le montant produit est superieur a la capacite disponible
-            if (produitFermier.getAmount() > capaciteRestante) {
-                //on calcul le nombre de produit détruit par l'operation
-                int deletedAmount = produitFermier.getAmount() - capaciteRestante;
-                //on remplace le montant de produit par un montant qui rentre dans la capacite du producteur
-                produitFermier.setAmount(capaciteRestante);
-                //on leve une exception comme quoi des produits ont été perdus pour manque de place
-                throw new NotEnoughCapacityException(produitFermier,deletedAmount);
-            }
-            if (bio) {
-                produitFermier.setBio(true);
-            }
-            produitFermier.setProprietaire(this);
-            super.getStock().add(produitFermier);
+        if (produitFermier.getAmount() > capaciteRestante) {
+            //on calcul le nombre de produit détruit par l'operation
+            int deletedAmount = produitFermier.getAmount() - capaciteRestante;
+            //on remplace le montant de produit par un montant qui rentre dans la capacite du producteur
+            produitFermier.setAmount(capaciteRestante);
+            //on leve une exception comme quoi des produits ont été perdus pour manque de place
+            throw new NotEnoughCapacityException(produitFermier,deletedAmount);
         }
+
+        if (bio) {
+            produitFermier.setBio(true);
+        }
+        super.addProduit(produitFermier);
+
+
+
     }
 
     private int getCurrentCapacite() {
@@ -61,20 +60,7 @@ public class Producteur extends Acheteur {
         return currentCapacite;
     }
 
-    private boolean addExistingProduit(ProduitFermier produitFermier) {
-        //si le produit est deja en possession du producteur
-        for (ProduitFermier currentProduit : super.getStock()) {
-            //si le nom du nouveau produit correspond au nom d'un produit du stock
-            if (currentProduit.getName().equalsIgnoreCase(produitFermier.getName())) {
-                //on change juste le montant
-                int currentAmount = currentProduit.getAmount();
-                currentProduit.setAmount(currentAmount + produitFermier.getAmount());
-                return true;
-            }
-        }
-        //si le produit n'existe pas dans le stock
-        return false;
-    }
+
 
     public void setBio(boolean bio) {
         this.bio = bio;
