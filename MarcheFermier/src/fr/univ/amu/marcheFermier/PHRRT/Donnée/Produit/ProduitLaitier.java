1246597f3492c50a1c42d5f701d85fdaa7eb3f8f
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 13:06:12 +0100

    Javadoc de ProduitLaitier

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitLaitier.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitLaitier.java
@@ -5,6 +5,15 @@ import fr.univ.amu.marcheFermier.PHRRT.Donnée.Acheteur;
 import java.time.LocalDate;
 
 public abstract class ProduitLaitier extends ProduitFermier {
+
+    /**
+     * Constructeur ProduitLaitier.
+     *
+     * @param peremptionDate
+     * @param name
+     * @param amount
+     * @param proprietaire
+     */
     public ProduitLaitier(LocalDate peremptionDate, String name, int amount, Acheteur proprietaire) {
         super(peremptionDate,name, amount, proprietaire);
     }
