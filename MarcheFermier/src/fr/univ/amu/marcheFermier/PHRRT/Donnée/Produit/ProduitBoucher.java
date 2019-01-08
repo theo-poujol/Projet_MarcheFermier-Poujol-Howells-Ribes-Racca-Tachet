0ee2d113b36aa4443206e978232f0e6a19857698
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 13:05:36 +0100

    Javadoc de ProduitBoucher

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitBoucher.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitBoucher.java
@@ -6,6 +6,14 @@ import java.time.LocalDate;
 
 public abstract class ProduitBoucher extends ProduitFermier {
 
+    /**
+     * Constructeur ProduitBoucher.
+     *
+     * @param peremptionDate
+     * @param name
+     * @param amount
+     * @param proprietaire
+     */
     public ProduitBoucher(LocalDate peremptionDate, String name, int amount, Acheteur proprietaire) {
         super(peremptionDate, name, amount, proprietaire);
     }
