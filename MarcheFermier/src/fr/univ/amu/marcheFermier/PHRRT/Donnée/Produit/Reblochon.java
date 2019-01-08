4b16a2c93f04d6be74583513b18c80346961ea46
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 13:06:35 +0100

    Javadoc de Reblochon

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/Reblochon.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/Reblochon.java
@@ -6,7 +6,13 @@ import java.time.LocalDate;
 
 public class Reblochon extends ProduitFromager {
 
-
+    /**
+     * Constructeur Reblochon.
+     *
+     * @param peremptionDate
+     * @param amount
+     * @param proprietaire
+     */
     public Reblochon(LocalDate peremptionDate, int amount, Acheteur proprietaire) {
         super(peremptionDate,"Reblochon", amount, proprietaire);
     }
