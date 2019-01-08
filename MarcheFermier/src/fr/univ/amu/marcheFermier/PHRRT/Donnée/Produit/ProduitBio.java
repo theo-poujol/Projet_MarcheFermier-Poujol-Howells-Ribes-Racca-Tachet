eb574702d2a56876bdb4c2f284dae9e85fe9a01f
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 13:05:22 +0100

    Javadoc de ProduitBio

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitBio.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitBio.java
@@ -6,7 +6,14 @@ import java.time.LocalDate;
 
 public abstract class ProduitBio extends ProduitFermier {
 
-
+    /**
+     * Constructeur ProduitBio.
+     *
+     * @param peremptionDate
+     * @param name
+     * @param amount
+     * @param proprietaire
+     */
     public ProduitBio(LocalDate peremptionDate, String name, int amount, Acheteur proprietaire) {
         super(peremptionDate,name, amount, proprietaire);
     }
