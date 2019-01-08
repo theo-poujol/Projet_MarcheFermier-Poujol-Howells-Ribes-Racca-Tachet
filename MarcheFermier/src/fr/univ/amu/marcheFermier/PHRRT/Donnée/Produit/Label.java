27d49a7846abf760140f11023fe9f9eaca3a952d
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 13:03:31 +0100

    Javadoc de Label

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/Label.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/Label.java
@@ -4,15 +4,31 @@ public class Label {
     private String title;
     private String description;
 
+    /**
+     * Constructeur Label.
+     *
+     * @param title
+     * @param description
+     */
     public Label(String title, String description) {
         this.title = title;
         this.description = description;
     }
 
+    /**
+     * Getter qui retourne un titre.
+     *
+     * @return title
+     */
     public String getTitle() {
         return title;
     }
 
+    /**
+     * Getter qui retourne une description.
+     *
+     * @return description
+     */
     public String getDescription() {
         return description;
     }
