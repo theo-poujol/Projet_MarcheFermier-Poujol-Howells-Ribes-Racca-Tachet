fe1be7deb668b2ace6747594fcdb5316edbb99f0
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 12:18:14 +0100

    Javadoc de PropositionVente

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Trade/PropositionVente.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Trade/PropositionVente.java
@@ -8,7 +8,10 @@ import fr.univ.amu.marcheFermier.PHRRT.Traitement.Controleur;
 
 import java.util.ArrayList;
 
-public class PropositionVente { //Une Proposition de vente terminée devient une Transaction, qui sera elle-même insérée dans le Livre de Marché
+/**
+ * Une Proposition de vente terminée devient une Transaction, qui sera elle-même insérée dans le Livre de Marché
+ */
+public class PropositionVente {
 
     private Acheteur monVendeur; //Qui vend
     private ProduitFermier monProduit; // Ce qu'il vend
@@ -21,7 +24,13 @@ public class PropositionVente { //Une Proposition de vente terminée devient une
     }
 
 
-
+    /**
+     * Constructeur PropositionVente.
+     *
+     * @param monVendeur
+     * @param monProduit
+     * @param prix
+     */
     public PropositionVente(Producteur monVendeur, ProduitFermier monProduit, int prix) {
 
         this.monVendeur = monVendeur;
@@ -46,43 +55,96 @@ public class PropositionVente { //Une Proposition de vente terminée devient une
 
 
 
+    /**
+     * Getter getMonVendeur retourne "monVendeur".
+     *
+     * @return monVendeur qui est la personne qui vend le produit.
+     */
 
     public Acheteur getMonVendeur() {
         return monVendeur;
     }
 
+    /**
+     *Setter setMonVendeur qui set le monVendeur
+     *
+     * @param monVendeur
+     */
     public void setMonVendeur(Producteur monVendeur) {
         this.monVendeur = monVendeur;
     }
 
+    /**
+     *getter getMonProduit qui retourne le produit qu'il vend.
+     *
+     * @return monProduit
+     */
     public ProduitFermier getMonProduit() {
         return monProduit;
     }
 
+    /**
+     *Setter setMonProduit
+     *
+     * @param monProduit
+     */
     public void setMonProduit(ProduitFermier monProduit) {
         this.monProduit = monProduit;
     }
 
+    /**
+     *getter getPrix qui return le prix du produit.
+     *
+     * @return prix
+     *          prix est le prix du produit.
+     */
     public Double getPrix() {
         return prix;
     }
 
+    /**
+     * setter setPrix qui set le prix du produit.
+     *
+     * @param prix
+     *        prix est le prix du produit.
+     */
     public void setPrix(Double prix) {
         this.prix = prix;
     }
 
+    /**
+     *getter getMesAcheteurs qui retourne les acheteurs.
+     *
+     * @return mesAcheteurs.
+     */
     public ArrayList<Acheteur> getMesAcheteurs() {
         return mesAcheteurs;
     }
 
+    /**
+     *setter setMesAcheteurs
+     *
+     * @param mesAcheteurs
+     */
     public void setMesAcheteurs(ArrayList<Acheteur> mesAcheteurs) {
         this.mesAcheteurs = mesAcheteurs;
     }
 
+    /**
+     *isValid est un boolean qui permet a l'AMF de rendre une annonce visible ou non.
+     *
+     * @return isValid
+     */
     public boolean isValid() {
         return isValid;
     }
 
+    /**
+     *setter setValid qui decide si l'annonce est visible ou non.
+     *
+     * @param valid
+     *          si l'AMF valide l'annonce, celle ci devient visible
+     */
     public void setValid(boolean valid) {
         isValid = valid;
     }
