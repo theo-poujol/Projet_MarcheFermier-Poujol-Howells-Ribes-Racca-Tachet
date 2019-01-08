596080821836fe1f0d13c1e04522eb1ff7f6987b
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 13:06:00 +0100

    Javadoc de ProduitFromager

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitFromager.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitFromager.java
@@ -6,6 +6,15 @@ import java.time.LocalDate;
 
 public abstract class ProduitFromager extends ProduitFermier {
 
+    /**
+     * Constructeur ProduitFromager.
+     *
+     * @param peremptionDate
+     * @param name
+     * @param amount
+     * @param proprietaire
+     */
+
     public ProduitFromager(LocalDate peremptionDate, String name, int amount, Acheteur proprietaire) {
         super(peremptionDate, name, amount,proprietaire);
     }
