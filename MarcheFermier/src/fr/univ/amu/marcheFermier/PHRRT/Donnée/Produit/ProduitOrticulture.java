867d340474f920a518232dfcbaf6f7a276d58652
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 13:06:23 +0100

    Javadoc de ProduitOrticulture

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitOrticulture.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitOrticulture.java
@@ -5,6 +5,15 @@ import fr.univ.amu.marcheFermier.PHRRT.Donnée.Acheteur;
 import java.time.LocalDate;
 
 public class ProduitOrticulture extends ProduitFermier {
+
+    /**
+     * Constructeur ProduitOrticulture.
+     *
+     * @param peremptionDate
+     * @param name
+     * @param amount
+     * @param proprietaire
+     */
     public ProduitOrticulture(LocalDate peremptionDate, String name, int amount, Acheteur proprietaire) {
         super(peremptionDate, name, amount, proprietaire);
     }
