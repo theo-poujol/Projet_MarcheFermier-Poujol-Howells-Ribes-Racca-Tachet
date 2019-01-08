54c3367f5898e2168980201be7cc220707983b82
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 13:06:46 +0100

    Javadoc de Tomate

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/Tomate.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/Tomate.java
@@ -5,6 +5,14 @@ import fr.univ.amu.marcheFermier.PHRRT.Donnée.Acheteur;
 import java.time.LocalDate;
 
 public class Tomate extends ProduitOrticulture {
+
+    /**
+     * Constructeur Tomate.
+     *
+     * @param peremptionDate
+     * @param amount
+     * @param proprietaire
+     */
     public Tomate(LocalDate peremptionDate, int amount, Acheteur proprietaire) {
         super(peremptionDate, "Tomate", amount, proprietaire);
     }
