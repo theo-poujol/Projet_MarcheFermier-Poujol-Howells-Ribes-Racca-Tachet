8c0d34bd5a1b8ca805c91c787390781fdb504e16
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 13:04:55 +0100

    Javadoc de ProduitApiculture

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitApiculture.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitApiculture.java
@@ -6,6 +6,15 @@ import java.time.LocalDate;
 
 public abstract class ProduitApiculture extends ProduitFermier {
 
+    /**
+     * Constructeur ProduitApiculture.
+     *
+     * @param peremptionDate
+     * @param name
+     * @param amount
+     * @param proprietaire
+     */
+
     public ProduitApiculture(LocalDate peremptionDate, String name, int amount, Acheteur proprietaire) {
         super(peremptionDate,name, amount,proprietaire);
     }
