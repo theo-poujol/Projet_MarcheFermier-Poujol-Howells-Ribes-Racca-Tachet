8e34653ec1c8a0a750cae6a90692540ec8aec57f
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 13:05:10 +0100

    Javadoc de ProduitArboricole

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitArboricole.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/ProduitArboricole.java
@@ -5,6 +5,15 @@ import fr.univ.amu.marcheFermier.PHRRT.Donnée.Acheteur;
 import java.time.LocalDate;
 
 public abstract class ProduitArboricole extends ProduitFermier{
+
+    /**
+     * Constructeur ProduitArboricole.
+     *
+     * @param peremptionDate
+     * @param name
+     * @param amount
+     * @param proprietaire
+     */
     public ProduitArboricole(LocalDate peremptionDate, String name, int amount, Acheteur proprietaire) {
         super(peremptionDate, name, amount, proprietaire);
     }
