b50c493067b5b0af29da8459227be9ed38e61cd0
loris-racca <loris.racca@etu.univ-amu.fr>
2019-01-08 13:03:20 +0100

    Javadoc de Iris

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/Iris.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Donnée/Produit/Iris.java
@@ -5,6 +5,14 @@ import fr.univ.amu.marcheFermier.PHRRT.Donnée.Acheteur;
 import java.time.LocalDate;
 
 public class Iris extends ProduitOrticulture {
+
+    /**
+     * Constructeur Iris.
+     *
+     * @param peremptionDate
+     * @param amount
+     * @param proprietaire
+     */
     public Iris(LocalDate peremptionDate,int amount, Acheteur proprietaire) {
         super(peremptionDate, "Iris", amount, proprietaire);
     }
