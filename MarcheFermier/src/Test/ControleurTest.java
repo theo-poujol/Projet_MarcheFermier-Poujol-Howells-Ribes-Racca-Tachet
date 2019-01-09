15769b980919574d01d7886171e8dffb2dbab598
dorian-tachet <dorian.tachet@etu.univ-amu.fr>
2019-01-09 19:09:16 +0100

    test controleur

--- MarcheFermier/src/Test/ControleurTest.java
+++ MarcheFermier/src/Test/ControleurTest.java
@@ -1,6 +1,12 @@
 package Test;
 
+import fr.univ.amu.marcheFermier.PHRRT.Donnée.Acheteur;
+import fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit.Miel;
+import fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit.ProduitFermier;
+import fr.univ.amu.marcheFermier.PHRRT.Traitement.Controleur;
 import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,13 +14,25 @@ class ControleurTest {
 
     @Test
     void choisirAcheteur() {
+
     }
 
     @Test
     void inspecter() {
+
     }
 
     @Test
     void valider() {
+        Acheteur acheteur = new Acheteur(200,"loris");
+        ProduitFermier produitFermier = new Miel(LocalDate.now(),10, acheteur);
+
+        Controleur controleur = Controleur.getInstance();
+
+        controleur.valider(produitFermier);
+
+        if (!produitFermier.isValide()) {
+            fail();
+        }
     }
 }
