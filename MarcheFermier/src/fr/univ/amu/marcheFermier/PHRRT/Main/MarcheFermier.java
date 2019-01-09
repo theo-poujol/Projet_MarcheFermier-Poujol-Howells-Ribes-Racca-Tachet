c88e08b1bb3b810d600c25f6c1c898461a6e219f
dorian-tachet <dorian.tachet@etu.univ-amu.fr>
2019-01-09 15:45:50 +0100

    Initialisation du marché

--- MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Main/MarcheFermier.java
+++ MarcheFermier/src/fr/univ/amu/marcheFermier/PHRRT/Main/MarcheFermier.java
@@ -6,11 +6,7 @@ import fr.univ.amu.marcheFermier.PHRRT.Donnée.Producteur.Apiculteur;
 import fr.univ.amu.marcheFermier.PHRRT.Donnée.Producteur.Fromager;
 import fr.univ.amu.marcheFermier.PHRRT.Donnée.Producteur.Producteur;
 import fr.univ.amu.marcheFermier.PHRRT.Donnée.Producteur.ProducteurDeViande;
-import fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit.Miel;
-import fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit.ProduitFermier;
-import fr.univ.amu.marcheFermier.PHRRT.Donnée.Produit.ProduitLaitier;
-import fr.univ.amu.marcheFermier.PHRRT.Donnée.Trade.PropositionVente;
-import fr.univ.amu.marcheFermier.PHRRT.Traitement.Menu;
+import fr.univ.amu.marcheFermier.PHRRT.Donnée.Trade.Trader;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -32,8 +28,22 @@ public class MarcheFermier {
         participants.add(loris);
         participants.add(albert);
 
+
         Marche marche = new Marche("Loris",5);
         marche.setParticipants(participants);
+
+        Trader trader = new Trader(marche,"Vero");
+        trader.addClient(loris);
+        trader.addClient(albert);
+        trader.addClient(producteur);
+
+        Trader trader2 = new Trader(marche,"Nathalie");
+        trader2.addClient(loris);
+        trader2.addClient(albert);
+
+        marche.addTrader(trader);
+        marche.addTrader(trader2);
+
         marche.start();
 
     }
